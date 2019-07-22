9c187dc214ec91e745ce22c404bb8ae079e2a095
ekalem <ekalemdesign@gmail.com>
2019-07-22 18:23:33 +0300

    Form üye kayıt ve nesneleri yüklendi.

---
+++ form.uye.kayit.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Untitled Form</title>
+<link rel="stylesheet" type="text/css" href="form-nesneler/view.css" media="all">
+<script type="text/javascript" src="form-nesneler/view.js"></script>
+
+</head>
+<body id="main_body" >
+
+	<img id="top" src="form-nesneler/top.png" alt="">
+	<div id="form_container">
+
+		<h1><a>Untitled Form</a></h1>
+		<form id="form_74674" class="appnitro"  method="post" action="">
+					<div class="form_description">
+			<h2>Aç Adam</h2>
+			<p>Müşteri Kayıt Formu</p>
+		</div>
+			<ul >
+
+					<li id="li_1" >
+		<label class="description" for="element_1">Adı Soyadı </label>
+		<div>
+			<input id="element_1" name="element_1" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_2" >
+		<label class="description" for="element_2">ePosta </label>
+		<div>
+			<input id="element_2" name="element_2" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_3" >
+		<label class="description" for="element_3">Parola </label>
+		<div>
+			<input id="element_3" name="element_3" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_4" >
+		<label class="description" for="element_4">Parola Onay </label>
+		<div>
+			<input id="element_4" name="element_4" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_5" >
+		<label class="description" for="element_5">Telefon </label>
+		<div>
+			<input id="element_5" name="element_5" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_12" >
+		<label class="description" for="element_12">İl </label>
+		<div>
+		<select class="element select medium" id="element_12" name="element_12">
+			<option value="" selected="selected"></option>
+<option value="1" >Adana</option>
+<option value="2" >Adıyaman</option>
+<option value="3" >Afyon</option>
+
+		</select>
+		</div>
+		</li>		<li id="li_13" >
+		<label class="description" for="element_13">İlçe </label>
+		<div>
+		<select class="element select medium" id="element_13" name="element_13">
+			<option value="" selected="selected"></option>
+<option value="1" >Ceyhan</option>
+<option value="2" >Adana Merkez</option>
+<option value="3" >Adıyaman Merkez</option>
+
+		</select>
+		</div>
+		</li>		<li id="li_6" >
+		<label class="description" for="element_6">Posta Kodu </label>
+		<div>
+			<input id="element_6" name="element_6" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_14" >
+		<label class="description" for="element_14">Mahalle </label>
+		<div>
+		<select class="element select medium" id="element_14" name="element_14">
+			<option value="" selected="selected"></option>
+<option value="1" >Huzur</option>
+<option value="2" >Abant</option>
+<option value="3" >Merkez</option>
+
+		</select>
+		</div>
+		</li>		<li id="li_7" >
+		<label class="description" for="element_7">Cadde </label>
+		<div>
+			<input id="element_7" name="element_7" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_8" >
+		<label class="description" for="element_8">Sokak </label>
+		<div>
+			<input id="element_8" name="element_8" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_9" >
+		<label class="description" for="element_9">Bina No </label>
+		<div>
+			<input id="element_9" name="element_9" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_10" >
+		<label class="description" for="element_10">Kapı No </label>
+		<div>
+			<input id="element_10" name="element_10" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_11" >
+		<label class="description" for="element_11">Ek Bilgi / Adres Tarifi </label>
+		<div>
+			<input id="element_11" name="element_11" class="element text medium" type="text" maxlength="255" value=""/>
+		</div>
+		</li>		<li id="li_15" >
+		<label class="description" for="element_15">Kullanıcı Sözleşmesi </label>
+		<span>
+			<input id="element_15_1" name="element_15_1" class="element checkbox" type="checkbox" value="1" />
+<label class="choice" for="element_15_1">Kabul Ediyorum</label>
+
+		</span>
+		</li>
+
+					<li class="buttons">
+			    <input type="hidden" name="form_id" value="74674" />
+
+				<input id="saveForm" class="button_text" type="submit" name="submit" value="KAYDOL" />
+		</li>
+			</ul>
+		</form>
+		<div id="footer">
+			Aç Adam - 2019
+		</div>
+	</div>
+	<img id="bottom" src="form-nesneler/bottom.png" alt="">
+	</body>
+</html>
