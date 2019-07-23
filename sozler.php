096d52606c80eb6e1e46777dd246f9901920acd7
ekalem <ekalemdesign@gmail.com>
2019-07-23 14:34:36 +0300

    duzeltildi. calisan

--- sozler.php
+++ sozler.php
@@ -4,17 +4,17 @@ $sozler = array(
   "Bir elin nesi var",
   "Birimiz hepimiz için",
   "Saman altından su yürütmek",
-  "Sora sora Bağdat bulunur";
-)
+  "Sora sora Bağdat bulunur"
+);
 
 echo $sozler[2];
 
-veya
+// veya
 
 for ($i=1; $i<=10; $i++) {
-  $SozNo = rand(0, 3);
-  echo $sozler[ $SozNo ] . "<br />";
+  $x = rand(0, 3);
+  echo $sozler[ $x ] . "<br />";
 }
 
-
+phpversion();
  ?>
